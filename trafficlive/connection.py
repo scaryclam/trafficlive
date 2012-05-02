5e58169221033e60109366ee68f9cc78c6e75b22
Becky <blewis@becky-dev.tangentuk.local>
2012-05-02 15:56:06 +0100

    Made a start on implimenting metaclass

--- trafficlive/connection.py
+++ trafficlive/connection.py
@@ -1,34 +1,19 @@
 import requests
 
 
+class _Connection(type):
+    def __new__(cls, clsname, clsbases, clsdict):
+        t = type.__new__(cls, clsname, clsbases, clsdict)
+        t.__foo__ = "Hello World"
+        return t
+
+
 class Connection(object):
+    __metaclass__ = _Connection
+
     def __init__(self, host="https://api.sohnar.com/TrafficLiteServer/openapi", username=None, password=None):
         self.conn = requests.session()
         self.host = host
-
-    def get_employees(self):
-        url = "/staff/employee"
-        self._send_request(self, url, "GET")
-
-    def get_employee(self, employee_id):
-        url = "/staff/employee/%s" % str(employee_id)
-        self._send_request(self, url, "GET")
-
-    def get_departments(self):
-        url = "/staff/department"
-        self._send_request(self, url, "GET")
-
-    def get_department(self, dept_id):
-        url = "/staff/department/%s" % str(dept_id)
-        self._send_request(self, url, "GET")
-
-    def get_locations(self):
-        url = "/staff/location"
-        self._send_request(self, url, "GET")
-
-    def get_location(self, location_id):
-        url = "/staff/location/%s" % str(location_id)
-        self._send_request(self, url, "GET")
 
     def _send_request(self, url, method):
         func = getattr(self.conn, method)
