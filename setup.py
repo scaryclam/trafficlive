4a7bf4b136d2ede962073ceb018e152f275f4b6b
Becky Lewis <github@scaryclam.co.uk>
2012-06-05 14:10:44 +0100

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "trafficlive",
+    version = "0.1",
+    author = "Becky Lewis",
+    author_email = "trafficlive@scaryclam.co.uk",
+    description = ("A simple API wrapper for trafficlives open API"),
+    license = "BSD",
+    keywords = "traficlive",
+    url = "https://github.com/scaryclam/trafficlive",
+    packages=['trafficlive'],
+    long_description=read('README.txt'),
+)
+
